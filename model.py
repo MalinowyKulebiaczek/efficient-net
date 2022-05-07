3d51a43212f742ac1a03eeff63d38cf86e72de08
Rafał Wiercioch <rafa.wiercioch@gmail.com>
2022-05-08 00:52:13 +0200

    added implementation of efficient net + some minor bug fixes

--- model.py
+++ model.py
@@ -65,18 +65,18 @@ class InvertedResidualBlock(nn.Module):
                  reduction=4,  # squeeze excitation
                  survival_prob=0.8):  # stochastic depth
         super(InvertedResidualBlock, self).__init__()
-        self.survival_prob = 0.8
+        self.survival_prob = survival_prob
         self.use_residual = in_channels == out_channels and stride == 1
         hidden_dim = in_channels * expand_ratio
         self.expand = in_channels != hidden_dim
-        reduced_dim = hidden_dim // reduction  # originally in_channels/reduction
+        reduced_dim = int(hidden_dim / reduction)  # int(in_channels/reduction) #
         if self.expand:
-            self.expand_conv = CNNBlock(in_channels, hidden_dim, kernel_size=1, stride=1, padding=1)
+            self.expand_conv = CNNBlock(in_channels, hidden_dim, kernel_size=3, stride=1, padding=1)  # kernel_size 1?
 
         self.conv = nn.Sequential(
             CNNBlock(hidden_dim, hidden_dim, kernel_size, stride, padding, groups=hidden_dim),
             SqueezeExcitation(hidden_dim, reduced_dim),
-            nn.Conv2d(hidden_dim, out_channels, bias=False),
+            nn.Conv2d(hidden_dim, out_channels, 1, bias=False),
             nn.BatchNorm2d(out_channels)
         )
 
@@ -97,4 +97,62 @@ class InvertedResidualBlock(nn.Module):
 
 
 class EfficientNet(nn.Module):
-    pass
+    def __init__(self, version, num_classes):
+        super(EfficientNet, self).__init__()
+        width_factor, depth_factor, dropout_rate = self.calculate_factors(version)
+        last_channels = ceil(1280 * width_factor)
+        self.pool = nn.AdaptiveAvgPool2d(1)
+        self.features = self.create_features(width_factor, depth_factor, last_channels)
+        self.classifier = nn.Sequential(
+            nn.Dropout(dropout_rate),
+            nn.Linear(last_channels, num_classes)
+        )
+
+    def calculate_factors(selfself, version, alpha=1.2, beta=1.1):
+        phi, res, drop_rate = phi_values[version]
+        depth_factor = alpha ** phi
+        width_factor = beta ** phi
+        return width_factor, depth_factor, drop_rate
+
+    def create_features(self, width_factor, depth_factor, last_channels):
+        channels = int(32 * width_factor)
+        features = [CNNBlock(3, channels, 3, stride=2, padding=1)]
+        in_channels = channels
+
+        for expand_ratio, channels, repeats, stride, kernel_size in base_model:
+            out_channels = 4 * ceil(int(channels * width_factor) / 4)
+            layers_repeats = ceil(repeats * depth_factor)
+
+            for layer in range(layers_repeats):
+                features.append(
+                    InvertedResidualBlock(in_channels, out_channels, expand_ratio=expand_ratio,
+                                          stride=stride if layer == 0 else 1,
+                                          kernel_size=kernel_size,
+                                          padding=kernel_size // 2)
+                )
+                in_channels = out_channels
+
+        features.append(CNNBlock(in_channels, last_channels, kernel_size=1, stride=1, padding=0))
+
+        return nn.Sequential(*features)
+
+    def forward(self, x):
+        x = self.pool(self.features(x))
+        return self.classifier(x.view(x.shape[0], -1))
+
+
+def test():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    version = "b0"
+    phi, res, drop_rate = phi_values[version]
+    num_examples, num_classes = 4, 10
+    x = torch.randn((num_examples, 3, res, res)).to(device)
+    model = EfficientNet(
+        version=version,
+        num_classes=num_classes,
+    ).to(device)
+
+    print(model(x).shape)  # (num_examples, num_classes)
+
+
+test()
